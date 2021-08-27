9ac1b8929daa6fe7fd38baf4fd72a4297bdc6272
Programster <sdpagent@gmail.com>
2021-08-27 17:22:45 +0100

    updates RequestedAttribute to make some of the fields optional, and default to not being set

--- src/RequestedAttribute.php
+++ src/RequestedAttribute.php
@@ -9,17 +9,25 @@ class RequestedAttribute
 {
     private string $m_name;
     private bool $m_isRequired;
-    public AttributeNameFormat $m_nameFormat;
-    public string $m_friendlyName;
-    public array $m_attributeValue;
+    public ?AttributeNameFormat $m_nameFormat;
+    public ?string $m_friendlyName;
+    public ?array $m_attributeValue;
 
 
+    /**
+     * 
+     * @param bool $isRequired - whether the attribute is required or not.
+     * @param string $name - the formal/computer name for the attribute. E.g. "urn:oid:1.3.6.1.4.1.1466.115.121.1.26"
+     * @param string|null $friendlyName - the friendly name for the attribute. E.g. "email"
+     * @param AttributeNameFormat|null $nameFormat - optionally set the format for the requested attribute
+     * @param array|null $attributeValue - optionally set the attribute value
+     */
     public function __construct(
-        string $name,
         bool $isRequired,
-        AttributeNameFormat $nameFormat,
-        string $friendlyName,
-        array $attributeValue
+        string $name,
+        ?string $friendlyName = null,
+        ?AttributeNameFormat $nameFormat = null,
+        ?array $attributeValue
     )
     {
         $this->m_name = $name;
@@ -32,12 +40,24 @@ class RequestedAttribute
 
     public function toArray()
     {
-        return [
-            "name" => $this->m_name,
+        $arrayFormat = [
             "isRequired" => $this->m_isRequired,
-            "nameFormat" => (string)$this->m_nameFormat,
-            "friendlyName" => $this->m_friendlyName,
-            "attributeValue" => $this->m_attributeValue,
+            "name" => $this->m_name,
         ];
+
+        if ($this->m_friendlyName !== null)
+        {
+            $arrayFormat["friendlyName"] = $this->m_friendlyName;
+        }
+
+        if ($this->m_nameFormat !== null)
+        {
+            $arrayFormat["nameFormat"] = (string)$this->m_nameFormat;
+        }
+
+        if ($this->m_attributeValue !== null)
+        {
+            $arrayFormat["attributeValue"] = $this->m_attributeValue;
+        }
     }
 }
