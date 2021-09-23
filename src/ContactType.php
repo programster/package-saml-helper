df398e5b7b0c3feda88cb24739eafa8b44ee527f
Programster <sdpagent@gmail.com>
2021-09-23 15:48:50 +0100

    fixes a bug causing the inability to set a support or administrative contact type.

--- src/ContactType.php
+++ src/ContactType.php
@@ -2,6 +2,7 @@
 
 /*
  * A class that acts as an enum (because PHP doesnt have enums yet), for the allowed contact types.
+ * More info: https://saml2.sustainsys.com/en/stable/config-elements/contact-person.html
  */
 
 declare(strict_types = 1);
@@ -21,8 +22,8 @@ final class ContactType implements \Stringable
 
 
     public static function createTechnical() : ContactType { return new ContactType("technical"); }
-    public static function createSupport() : ContactType { return new ContactType("technical"); }
-    public static function createAdministrative() : ContactType { return new ContactType("technical"); }
+    public static function createSupport() : ContactType { return new ContactType("support"); }
+    public static function createAdministrative() : ContactType { return new ContactType("administrative"); }
     public static function createBilling() : ContactType { return new ContactType("billing"); }
     public static function createOther() : ContactType { return new ContactType("other"); }
 
